25e7af5407b1399177f521c8c9edfc840bd15923
Dmitriy Solyshko <dmitriy.solyshko@mail.ru>
2023-10-26 21:24:11 +0300

    add lab5

---
+++ SP5.2/SP5.2/SP5.2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "ru");
+    float counter = 0;
+    mutex counterMutex;
+    thread thread1([&]() {
+        for (int i = 0; i < 100; ++i) {
+            counterMutex.lock();
+            counter += 2;
+            counterMutex.unlock();
+        }
+        });
+
+    thread thread2([&]() {
+        for (int i = 0; i < 100; ++i) {
+            counterMutex.lock();
+            counter -= 1;
+            counterMutex.unlock();
+        }
+        });
+
+    thread thread3([&]() {
+        for (int i = 0; i < 10; ++i) {
+            counterMutex.lock();
+            counter *= 1.3;
+            counterMutex.unlock();
+        }
+        });
+
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+    cout << "Значение переменной после операций " << counter << endl;
+
+    return 0;
+}
---
+++ SP5/SP5/SP5.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "ru");
+
+    atomic<float> counter(0);
+    thread thread1([&]() {
+        for (int i = 0; i < 100; ++i) {
+            counter.store(counter.load() + 2);;
+        }
+        });
+
+    thread thread2([&]() {
+        for (int i = 0; i < 100; ++i) {
+            counter.store(counter.load() - 1.0);
+        }
+        });
+
+    thread thread3([&]() {
+        for (int i = 0; i < 10; ++i) {
+            counter.store(counter.load() * 1.3);
+        }
+        });
+
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+    cout << "Значение переменной после операций " << counter.load() << endl;
+
+    return 0;
+}
